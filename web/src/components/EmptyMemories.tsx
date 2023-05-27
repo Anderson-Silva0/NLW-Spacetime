9c478d00daaf2f6bd72193dd8287d136291bdd86
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-05-27 16:42:40 -0300

    Adicionando os projetos da Cápsula do Tempo para web, mobile e backend desenvolvidos durante a NLW Spacetime da Rocketseat.

---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,15 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-16">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+          className="underline hover:text-gray-50"
+        >
+          criar agora
+        </a>
+      </p>
+    </div>
+  )
+}
